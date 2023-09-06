cbd9a5bc21546550159ae8ff84510322f8129ef4
TopDevelpoer <133781576+PETER990913@users.noreply.github.com>
2023-09-06 07:59:59 -0700

    'seven'

--- src/pages/inputTable/4_2.js
+++ src/pages/inputTable/4_2.js
@@ -15,28 +15,30 @@ function WasteAverage() {
                             <th>Average emission factor  of waste treatment method  (kg CO2e/tonne)</th>
                         </tr>
                         <tr>
-                            <td>Plastic</td>
-                            <td><input type='text' defaultValue="2000" className='Input_form' /></td>
+                            <td rowSpan={5}><input type='text' defaultValue="40" className='Input_form' /></td>
                             <td>Landfill</td>
-                            <td><input type='text' defaultValue='40' className='Input_form' /></td>
+                            <td><input type='text' defaultValue='25' className='Input_form' /></td>
+                            <td><input type='text' defaultValue="300" className='Input_form' /></td>
                         </tr>
                         <tr>
-                            <td>Plastic</td>
-                            <td><input type='text' defaultValue="5000" className='Input_form' /></td>
                             <td> Incinerated with energy recovery </td>
-                            <td><input type='text' defaultValue='2' className='Input_form' /></td>
+                            <td><input type='text' defaultValue='5' className='Input_form' /></td>
+                            <td><input type='text' defaultValue='0' className='Input_form' /></td>
                         </tr>
                         <tr>
-                            <td>Plastic</td>
-                            <td><input type='text' defaultValue="4000" className='Input_form' /></td>
                             <td> Recycled </td>
+                            <td><input type='text' defaultValue='30' className='Input_form' /></td>
+                            <td><input type='text' defaultValue='0' className='Input_form' /></td>
+                        </tr>
+                        <tr>
+                            <td> Recycled </td>
+                            <td><input type='text' defaultValue='20' className='Input_form' /></td>
                             <td><input type='text' defaultValue='10' className='Input_form' /></td>
                         </tr>
                         <tr>
-                            <td>Plastic</td>
-                            <td><input type='text' defaultValue="5000" className='Input_form' /></td>
-                            <td> Wastewater </td>
-                            <td><input type='text' defaultValue='0.5' className='Input_form' /></td>
+                            <td> Composted </td>
+                            <td><input type='text' defaultValue='20' className='Input_form' /></td>
+                            <td><input type='text' defaultValue='30' className='Input_form' /></td>
                         </tr>
                     </tbody>
                 </table>
